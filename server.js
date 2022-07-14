8190f9ae250d2c6fbb9d37468b9ab68e2cdd859f
William Huang <william.huang@fazzfinancial.com>
2022-07-14 17:02:53 +0800

    update

--- server.js
+++ server.js
@@ -4,10 +4,44 @@ const PORT = process.env.PORT || 80
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) =>
+  .get('/', (request, response) =>
   {
-    console.table(req)
-    res.send("ok")
+    const requestStart = Date.now();
+
+  let errorMessage = null;
+  let body = [];
+  request.on("data", chunk => {
+    body.push(chunk);
+  });
+  request.on("end", () => {
+    body = Buffer.concat(body);
+    body = body.toString();
+  });
+  request.on("error", error => {
+    errorMessage = error.message;
+  });
+
+  response.on("finish", () => {
+    const { rawHeaders, httpVersion, method, socket, url } = request;
+    const { remoteAddress, remoteFamily } = socket;
+
+    console.log(
+      JSON.stringify({
+        timestamp: Date.now(),
+        processingTime: Date.now() - requestStart,
+        rawHeaders,
+        body,
+        errorMessage,
+        httpVersion,
+        method,
+        remoteAddress,
+        remoteFamily,
+        url
+      })
+    );
+  });
+
+    response.send("ok")
   }
   )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
